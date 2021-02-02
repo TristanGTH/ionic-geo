4f21d51c26c9d6cfaedef008c5849ad760f1c71e
Tristan <gauthier.tristan@yahoo.fr>
2021-02-02 12:59:21 +0100

    first commit

---
+++ src/views/Commune.vue
@@ -0,0 +1,51 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Communes</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-title class="ion-margin">Code postal ou ville</ion-title>
+
+      <ion-input id="inputBar"></ion-input>
+      <div style="display: flex; justify-content: center; width: 100%">
+        <ion-button class="ion-margin-vertical" id="btn">Rechercher</ion-button>
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+
+
+<style scoped>
+
+  #inputBar{
+    background-color: white;
+    width: 80%;
+    margin: auto;
+    border-radius: 5px;
+    color: black;
+  }
+
+  #btn{
+    width: 40%;
+  }
+
+</style>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/vue';
+
+export default  {
+  name: 'Tab1',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput, IonButton }
+}
+</script>
+
--- src/views/Tabs.vue
+++ src/views/Tabs.vue
@@ -2,26 +2,26 @@
   <ion-page>
     <ion-tabs>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="tab1" href="/tabs/tab1">
+        <ion-tab-button tab="tab1" href="/tabs/communes">
           <ion-icon :icon="triangle" />
-          <ion-label>Tab 1</ion-label>
+          <ion-label>Commune</ion-label>
         </ion-tab-button>
           
-        <ion-tab-button tab="tab2" href="/tabs/tab2">
+        <ion-tab-button tab="tab2" href="/tabs/departements">
           <ion-icon :icon="ellipse" />
-          <ion-label>Tab 2</ion-label>
+          <ion-label>Département</ion-label>
         </ion-tab-button>
         
-        <ion-tab-button tab="tab3" href="/tabs/tab3">
+        <ion-tab-button tab="tab3" href="/tabs/regions">
           <ion-icon :icon="square" />
-          <ion-label>Tab 3</ion-label>
+          <ion-label>Région</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
   </ion-page>
 </template>
 
-<script lang="ts">
+<script>
 import { IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage } from '@ionic/vue';
 import { ellipse, square, triangle } from 'ionicons/icons';
 
