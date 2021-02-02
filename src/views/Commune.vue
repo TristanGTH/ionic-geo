991cc0b7ddf75a2de59990b9cd282a92c6269b10
Tristan <gauthier.tristan@yahoo.fr>
2021-02-02 15:06:39 +0100

    working fetch with code and name

--- src/views/Commune.vue
+++ src/views/Commune.vue
@@ -6,18 +6,41 @@
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
+          <ion-title class="">Code postal ou ville</ion-title>
         </ion-toolbar>
-      </ion-header>
-
-      <ion-title class="ion-margin">Code postal ou ville</ion-title>
 
       <ion-input id="inputBar"></ion-input>
       <div style="display: flex; justify-content: center; width: 100%">
-        <ion-button class="ion-margin-vertical" id="btn">Rechercher</ion-button>
+        <ion-button class="ion-margin-vertical" id="btn" @click="showCity">Rechercher</ion-button>
       </div>
+
+      <ion-card v-for="item in city" :key="item">
+        <ion-card-header style="display: flex; flex-direction: column; align-items: center">
+          <ion-card-subtitle>{{item.code}}</ion-card-subtitle>
+          <ion-card-title>{{item.nom}}</ion-card-title>
+        </ion-card-header>
+
+        <ion-list>
+          <ion-item>
+            <ion-label>Département</ion-label>
+            <ion-label class="ion-text-end">ds4</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>Région</ion-label>
+            <ion-label class="ion-text-end">ds4</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>Code Postal</ion-label>
+            <ion-label class="ion-text-end">ds4</ion-label>
+          </ion-item>
+          <ion-item>
+            <ion-label>Population</ion-label>
+            <ion-label class="ion-text-end">ds4</ion-label>
+          </ion-item>
+        </ion-list>
+      </ion-card>
+
     </ion-content>
   </ion-page>
 </template>
@@ -41,11 +64,40 @@
 </style>
 
 <script>
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/vue';
+import axios from 'axios'
+
 
 export default  {
   name: 'Tab1',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput, IonButton }
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonInput, IonButton, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle },
+  data(){
+    return{
+      city: null,
+    }
+  },
+  methods:{
+
+    showCity(){
+      let buttonValue = document.getElementById('inputBar').value
+      let transformed = parseInt(buttonValue)
+      let queryName
+      if (Number.isInteger(transformed)){
+        queryName = 'codePostal'
+      }
+      else {
+        queryName = 'nom'
+      }
+      axios.get('https://geo.api.gouv.fr/communes?'+queryName+'='+buttonValue)
+              .then((response) =>{
+                console.log(response.data)
+                this.city = response.data
+              })
+              .catch(function (error) {
+                console.log(error)
+              })
+    }
+  }
 }
 </script>
 
